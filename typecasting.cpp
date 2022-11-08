1c5b48af974efa38f265acad0cef37df1f03f0f0
zxelex007 <107753091+zxelex007@users.noreply.github.com>
2022-11-08 10:46:48 +0530

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int c = 13;
+int main()
+{
+    // *************Reference Variables**************** 
+    // Rohan Das----> Monty -----> Rohu ------> Dangerous Coder
+    // float x = 455;
+    // float & y = x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
+
+    // Typecasting
+    // syntax --> datatype(varname)
+    float a = 2.35;
+    float b =  2.43;
+    cout<<"The integer value of a is :\n"<<int(a)<<"\n";
+    cout<<int(a + b); 
+}
---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*
+    syntax for declaring Variable:
+
+    Datatype - Int , Float , char , double , bool
+
+    datatype varname = value ;
+
+*/
+
+int main(){
+    int a = 6;// This is an integer variable
+    int b = 10;// This is an integer variable
+    cout<< "Hello world"<< a+b;
+    return 0;
+}
